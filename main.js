dda3c940dbb9ef987209706022a7af79343ddc65
yuliax000 <ArthyerX@gmail.com>
2025-10-01 14:18:49 +1000

    1

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 const modeButton = document.getElementById("modeButton");
-
+let islightMode = true;
 // function to toggle between dark mode and light mode
 // takes in bool : if true set to dark if false set to light
-function switchModes(toDark){
-  if(toDark){
+function switchModes(toDark) {
+  if (toDark) {
     modeButton.textContent = "Light Mode";
+    islightMode = false;
   } else {
     modeButton.textContent = "Dark Mode";
+    islightMode = true;
   }
 }
+
+modeButton.addEventListener("click", () => {
+  switchModes(islightMode);
+});
+
+
+// when page loads, test the current user color mode preference
+let checkDarkMode = window.matchMedia("(prefers-color-scheme:dar")
